c35979411cf55d6f647c700b923f9219b8c1693a
shivambhat45 <shivambhat45@gmail.com>
2021-04-30 00:01:44 +0530

    security in settings

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'k4hx$h3cn&v_t4og*k8yikrn2p^8m@&i^s0o7*u3uv5fnpv%4c'
-SECRET_KEY=os.environ.get('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE')=='True')
